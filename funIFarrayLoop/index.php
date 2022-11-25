28fdd87f7da71bd7a4872ab5b9526e70cfcd17fc
MdMarufulislam <marufulislam911@gmail.com>
2022-11-25 23:37:17 +0600

    php

---
+++ funIFarrayLoop/index.php
@@ -0,0 +1,36 @@
+<?php
+
+function maruf(){
+
+  // $age = 40;
+
+  // if($age > 50){
+  //   echo "You are buira";
+  // }else{
+  //   echo "You are juiyan";
+  // }
+
+  // for ($i=0; $i < 10; $i++) { 
+  //   echo "The number is: $i <br>";
+  // }
+
+
+  $a = array(12, 13, 15, 60);
+  // echo array_sum($a);
+  // echo $a[0];
+  // echo $a[2];
+  // array_push($a,"red","white");
+  // array_pop($a);
+  print_r ($a);
+
+
+ 
+} maruf()
+
+
+
+
+
+
+
+?>
